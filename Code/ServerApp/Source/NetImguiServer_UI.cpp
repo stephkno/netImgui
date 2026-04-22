ce020ecd40fb3569cf6d906f4c585e5f8b081c7d
stephen <stephkno@gmail.com>
2026-04-21 18:17:16 -0700

    *

--- Code/ServerApp/Source/NetImguiServer_UI.cpp
+++ Code/ServerApp/Source/NetImguiServer_UI.cpp
@@ -944,7 +944,7 @@ ImVec4 DrawImguiContent()
 	Popup_ConfirmDisconnect();
 	//Popup_AboutNetImgui();
 
-//	DrawImguiContent_MainMenu();
+	DrawImguiContent_MainMenu();
 	DrawImguiContent_SetupDocking();
 	DrawImguiContent_Clients();
 	//ImGui::ShowDemoWindow();
